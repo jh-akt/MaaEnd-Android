d916fc0881d72c170c9f09db85c98cb61b164daa
jh-akt <jh-akt@users.noreply.github.com>
2026-05-02 20:54:06 +0800

    Release 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.maaend.android"
         minSdk = 30
         targetSdk = 36
-        versionCode = 10003
-        versionName = "1.0.3"
+        versionCode = 10004
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
